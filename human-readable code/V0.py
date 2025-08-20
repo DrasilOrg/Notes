6a1067dac2f127469049bb6e285d6179da64f754
Jason Balaci <jason@balaci.me>
2025-08-20 13:50:24 -0400

    V0.py: Replace latin variables with some greek where common, and replace others with confusing latin ones.

--- human-readable code/V0.py
+++ human-readable code/V0.py
@@ -2,18 +2,18 @@
 #
 # "Works on my machine"
 #
-# 4.9 replaces the `2.0 * ... / 9.8` components of the normal equation
+# `/ 4.9`` replaces `2.0 * ... / 9.8` in the formula for `t`
 
-v = float(input("Launch velocity?"))
-a = float(input("Launch angle?"))
+s = float(input("Launch velocity?"))
+Θ = float(input("Launch angle?"))
 import math
-t = v * math.sin(a) / 4.9
+t = s * math.sin(Θ) / 4.9
 f = float(input("Target distance?"))
-o = t * v * math.cos(a) - f
+Δ = t * s * math.cos(Θ) - f
 print("Flight time:", t)
-if math.fabs(o / f) < 2.0e-2:
+if math.fabs(Δ / f) < 2.0e-2:
     print("Hit.")
-elif o < 0.0:
+elif Δ < 0.0:
     print("Fell short.")
 else:
     print("Went long.")
